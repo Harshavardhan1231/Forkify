e048c2bcdacbd923922112429d767580c148a107
Harshavardhan1231 <harshavardhan.lokesh@gmail.com>
2022-09-21 13:56:42 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1474c43c-768c-4ca3-adb1-b9516f96223a';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,53 @@
+import View from './View';
+import icons from 'url:../../img/icons.svg';
+
+class Pagination extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = Number(this._data.page);
+    const nextButton = `<button data-goto="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--next">
+    <span>Page ${curPage + 1}</span>
+    <svg class="search__icon">
+      <use href="${icons}#icon-arrow-right"></use>
+    </svg>
+  </button>`;
+    const prevButton = `<button data-goto="${
+      curPage - 1
+    }" class="btn--inline pagination__btn--prev">
+  <svg class="search__icon">
+    <use href="${icons}#icon-arrow-left"></use>
+  </svg>
+  <span>Page ${curPage - 1}</span>
+</button>`;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) return nextButton;
+
+    // Page 1, and there are no other pages
+    if (curPage === 1 && numPages === 1) return ``;
+
+    // Last page
+    if (curPage === numPages) return prevButton;
+
+    // Other page
+    if (curPage < numPages) return prevButton + nextButton;
+  }
+}
+
+export default new Pagination();
